da86fa82f82399c3b594df5185a6c6953f55ada8
Muharrem Yaman <56295267+muharremyaman@users.noreply.github.com>
2022-12-16 15:01:18 +0300

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,21 @@
+package week2Assingment;
+
+public class Course {
+
+		public Course(int id,String name,String department, String teacherName,String className)
+		{
+			this.id=id;
+			this.name=name;
+			this.department=department;
+			this.teacherName=teacherName;
+			this.className=className;
+		}
+		
+		
+		
+		int id;
+		String name;
+		String department;
+		String teacherName;
+		String className;
+}
---
+++ Student.java
@@ -0,0 +1,25 @@
+package week2Assingment;
+
+public class Student {
+	
+	public Student(int id,String name, String surname, int age, char gender) {
+		
+	this.id=id;
+	this.name=name;
+	this.surname=surname;
+	this.age=age;
+	this.gender=gender;
+	
+	}
+	
+	
+	
+	
+	
+	int id;
+	String name;
+	String surname;
+	int age;
+	char gender;
+
+}
